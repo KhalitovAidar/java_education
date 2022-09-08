1f021138fcf58169c17fa802cbdb43ff16f6c405
KhalitovAidar <khalitovaidar2404@gmail.com>
2022-09-08 15:58:52 +0300

    first_commit

---
+++ FirstProgramm.java
@@ -0,0 +1,8 @@
+class FirstProgramm {
+    public static void main(String[] args) {
+        System.out.println("Khalitov Aidar Marsovich"); 
+        System.out.println("205");
+        System.out.println("ITIS");
+        System.out.println("KFU");
+    }
+}
